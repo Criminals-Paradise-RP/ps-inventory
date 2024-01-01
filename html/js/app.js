53926d1b570287fbcc8bdd24884bb22107f23fc6
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-01-01 18:22:30 -0500

    Update app.js

--- html/js/app.js
+++ html/js/app.js
@@ -522,14 +522,19 @@ function FormatItemInfo(itemData, dom) {
                 itemData.info.birthdate +
                 "</span></p>"
             );
-            // INSURANCE
+            // REBEL MONEYWASH
+        } else if (itemData.name == "wetbills") {
+            $(".item-info-title").html("<p>" + itemData.label + "</p>");
+            $(".item-info-description").html(
+                "<p><strong>Worth: </strong><span>$" +
+                itemData.info.worth +
+                "</span></p>"
+            );
         } else if (itemData.name == "insurance") {
             $(".item-info-title").html("<p>" + itemData.label + "</p>");
             $(".item-info-description").html(
                 "<p><strong>Name: </strong><span>" +
                 itemData.info.name +
-                "<p><strong>CitizenID: </strong><span>" +
-                itemData.info.citizenid +
                 "</span></p><p><strong>Plate: </strong><span>" +
                 itemData.info.plate +
                 "</span></p><p><strong>Vehicle Model: </strong><span>" +
@@ -540,28 +545,18 @@ function FormatItemInfo(itemData, dom) {
                 itemData.info.expire +
                 "</span></p>"
             );
-            // REBEL MONEYWASH
-        } else if (itemData.name == "wetbills") {
-            $(".item-info-title").html("<p>" + itemData.label + "</p>");
-            $(".item-info-description").html(
-                "<p><strong>Worth: </strong><span>$" +
-                itemData.info.worth +
-                "</span></p>"
-            );
         } else if (itemData.name == "car_registration") {
             $(".item-info-title").html("<p>" + itemData.label + "</p>");
             $(".item-info-description").html(
                 "<p><strong>Name: </strong><span>" +
                 itemData.info.name +
-                "<p><strong>CitizenID: </strong><span>" +
-                itemData.info.citizenid +
                 "</span></p><p><strong>Plate: </strong><span>" +
                 itemData.info.plate +
                 "</span></p><p><strong>Vehicle Model: </strong><span>" +
                 itemData.info.model +
                 "</span></p>"
             );
-            
+    
         } else if (itemData.name == "health_insurance") {
             $(".item-info-title").html("<p>" + itemData.label + "</p>");
             $(".item-info-description").html(
@@ -575,6 +570,19 @@ function FormatItemInfo(itemData, dom) {
                 itemData.info.expire +
                 "</span></p>"
             );
+        } else if (itemData.name == "home_insurance") {
+            $(".item-info-title").html("<p>" + itemData.label + "</p>");
+            $(".item-info-description").html(
+                "<p><strong>Name: </strong><span>" +
+                itemData.info.name +
+                "<p><strong>CitizenID: </strong><span>" +
+                itemData.info.citizenid +
+                "</span></p><p><strong>Date: </strong><span>" +
+                itemData.info.date +
+                "</span></p><p><strong>Expires: </strong><span>" +
+                itemData.info.expire +
+                "</span></p>"
+            );    
         } else if (itemData.name == "lawyerpass") {
             $(".item-info-title").html("<p>" + itemData.label + "</p>");
             $(".item-info-description").html(
